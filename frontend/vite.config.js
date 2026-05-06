972295bdc6267a1a7ce81b4ccaa055aed5e075b8
AlvaroCarrion <alvarocarrion.git@gmail.com>
2026-05-06 20:12:16 +0200

    Cambios de package*.json y vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-=======
 
 // https://vite.dev/config/
->>>>>>> feature/auth-login
 export default defineConfig({
   plugins: [react()],
 })
