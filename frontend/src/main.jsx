e836482da84a943caea7345666daf110db32c185
Irene <hiirelu@gmail.com>
2026-05-29 15:54:16 +0200

    fix: tokens globales se perdian en produccion (index.module.scss a index.scss)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import './index.module.scss'; // Estilos globales (variables, body, reset). Import por efecto secundario.
+import './index.scss'; // Estilos globales (variables, body, reset). Hoja GLOBAL, no CSS Module: si fuera .module.scss el build de producción se cargaría el bloque :root por tree-shaking y se perderían todas las variables.
 import App from './App.jsx'
 import AuthProvider from "./context/AuthProvider.jsx";
 import MessageProvider from "./context/MessageProvider.jsx";
